6ea738c83b57eb2a3eea5b5e5d6282e9ad354ed1
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-07-26 23:27:40 +0300

    add 16

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// let orderedQuantity = 6;
+// const pricePerDroid = 800;
+// const deliveryFee = 50;
+// let totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+// console.log(totalPrice);
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+// console.log(message);
+
 // 12
 function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
   totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
@@ -29,3 +37,31 @@ console.log(isValidPassword("kiwirul3z"));
 console.log(isValidPassword("jqueryismyjam"));
 // 15
 
+function checkAge(age) {
+  let message;
+  if (age >=  18) {
+    message = 'You are an adult';
+  } else {
+    message = 'You are a minor';
+  }
+  return message;
+}
+console.log(checkAge(20));
+console.log(checkAge(8));
+console.log(checkAge(14));
+console.log(checkAge(38));
+// 16
+function checkStorage(available, ordered) {
+  let message;
+   if (available >=  ordered) {
+    message = "Order is processed, our manager will contact you.";
+  } else {
+    message = "Not enough goods in stock!";
+  }
+  return message;
+}
+console.log(checkStorage(100, 50));
+console.log(checkStorage(100, 130));
+console.log(checkStorage(200, 20));
+console.log(checkStorage(200, 150));
+console.log(checkStorage(150, 180));

86f5cf4476e4334b5a8bb61ec9226d770a7cbed7
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-07-27 16:53:43 +0300

    add 20

--- js/script.js
+++ js/script.js
@@ -65,3 +65,57 @@ console.log(checkStorage(100, 130));
 console.log(checkStorage(200, 20));
 console.log(checkStorage(200, 150));
 console.log(checkStorage(150, 180));
+//18
+function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+  let message;
+  // Change code below this line
+  let totalPrice = pricePerDroid * orderedQuantity
+if (totalPrice > customerCredits) {
+    message = "Insufficient funds!";
+  } else {
+    message = `You ordered ${orderedQuantity} droids, you have ${customerCredits - totalPrice} credits left`;
+  }
+  return message;
+  // Change code above this line
+  return message;
+}
+console.log(makeTransaction(3000, 5, 23000));
+console.log(makeTransaction(1000, 3, 15000));
+console.log(makeTransaction(5000, 10, 8000));
+console.log(makeTransaction(2000, 8, 10000));
+console.log(makeTransaction(500, 10, 5000));
+// 19
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  let message;
+
+  if (password === null) {
+    message =  'Canceled by user!';
+  } else if (password === "jqueryismyjam") {
+    message = 'Welcome!';
+  } else {
+    message = 'Access denied, wrong password!';
+  }
+  return message;
+}
+console.log(checkPassword("mangohackzor"));
+console.log(checkPassword(null));
+console.log(checkPassword("polyhax"));
+console.log(checkPassword("jqueryismyjam"));
+// 20
+function checkStorage(available, ordered) {
+  let message;
+if (ordered === 0) {
+    message = "There are no products in the order!";
+  } else if (ordered > available) {
+    message = "Your order is too large, there are not enough items in stock!";
+  } else {
+    message = "The order is accepted, our manager will contact you";
+  }
+  return message;
+}
+console.log(checkStorage(100, 50));
+console.log(checkStorage(100, 130));
+console.log(checkStorage(70, 0));
+console.log(checkStorage(200, 20));
+console.log(checkStorage(200, 250));

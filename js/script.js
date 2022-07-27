c2eb68c3798724fa795cc71798db501cb4cfc135
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-07-28 00:00:48 +0300

    add 25

--- js/script.js
+++ js/script.js
@@ -7,115 +7,168 @@
 // console.log(message);
 
 // 12
-function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
-  totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
-  message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
-  return message;
-}
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+//   message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+//   return message;
+// }
 
-console.log(makeOrderMessage(2, 100, 50));
-console.log(makeOrderMessage(4, 300, 100));
-console.log(makeOrderMessage(10, 70, 200));
-// 13
-function isAdult(age) {
-  const passed = age >=  18;
-  return passed;
-}
+// console.log(makeOrderMessage(2, 100, 50));
+// console.log(makeOrderMessage(4, 300, 100));
+// console.log(makeOrderMessage(10, 70, 200));
+// // 13
+// function isAdult(age) {
+//   const passed = age >=  18;
+//   return passed;
+// }
 
-console.log(isAdult(20));
-console.log(isAdult(14));
-console.log(isAdult(8));
-console.log(isAdult(37));
-// 14
-function isValidPassword(password) {
-  const SAVED_PASSWORD = 'jqueryismyjam';
-  const isMatch = password === SAVED_PASSWORD;
-  return isMatch;
-}
-console.log(isValidPassword("mangodab3st"));
-console.log(isValidPassword("kiwirul3z"));
-console.log(isValidPassword("jqueryismyjam"));
-// 15
+// console.log(isAdult(20));
+// console.log(isAdult(14));
+// console.log(isAdult(8));
+// console.log(isAdult(37));
+// // 14
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = 'jqueryismyjam';
+//   const isMatch = password === SAVED_PASSWORD;
+//   return isMatch;
+// }
+// console.log(isValidPassword("mangodab3st"));
+// console.log(isValidPassword("kiwirul3z"));
+// console.log(isValidPassword("jqueryismyjam"));
+// // 15
 
-function checkAge(age) {
-  let message;
-  if (age >=  18) {
-    message = 'You are an adult';
-  } else {
-    message = 'You are a minor';
-  }
-  return message;
-}
-console.log(checkAge(20));
-console.log(checkAge(8));
-console.log(checkAge(14));
-console.log(checkAge(38));
-// 16
-function checkStorage(available, ordered) {
-  let message;
-   if (available >=  ordered) {
-    message = "Order is processed, our manager will contact you.";
-  } else {
-    message = "Not enough goods in stock!";
-  }
-  return message;
-}
-console.log(checkStorage(100, 50));
-console.log(checkStorage(100, 130));
-console.log(checkStorage(200, 20));
-console.log(checkStorage(200, 150));
-console.log(checkStorage(150, 180));
-//18
-function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
-  let message;
-  // Change code below this line
-  let totalPrice = pricePerDroid * orderedQuantity
-if (totalPrice > customerCredits) {
-    message = "Insufficient funds!";
-  } else {
-    message = `You ordered ${orderedQuantity} droids, you have ${customerCredits - totalPrice} credits left`;
-  }
-  return message;
-  // Change code above this line
-  return message;
-}
-console.log(makeTransaction(3000, 5, 23000));
-console.log(makeTransaction(1000, 3, 15000));
-console.log(makeTransaction(5000, 10, 8000));
-console.log(makeTransaction(2000, 8, 10000));
-console.log(makeTransaction(500, 10, 5000));
-// 19
-function checkPassword(password) {
-  const ADMIN_PASSWORD = 'jqueryismyjam';
-  let message;
+// function checkAge(age) {
+//   let message;
+//   if (age >=  18) {
+//     message = 'You are an adult';
+//   } else {
+//     message = 'You are a minor';
+//   }
+//   return message;
+// }
+// console.log(checkAge(20));
+// console.log(checkAge(8));
+// console.log(checkAge(14));
+// console.log(checkAge(38));
+// // 16
+// function checkStorage(available, ordered) {
+//   let message;
+//    if (available >=  ordered) {
+//     message = "Order is processed, our manager will contact you.";
+//   } else {
+//     message = "Not enough goods in stock!";
+//   }
+//   return message;
+// }
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 150));
+// console.log(checkStorage(150, 180));
+// //18
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   // Change code below this line
+//   let totalPrice = pricePerDroid * orderedQuantity
+// if (totalPrice > customerCredits) {
+//     message = "Insufficient funds!";
+//   } else {
+//     message = `You ordered ${orderedQuantity} droids, you have ${customerCredits - totalPrice} credits left`;
+//   }
+//   return message;
+//   // Change code above this line
+//   return message;
+// }
+// console.log(makeTransaction(3000, 5, 23000));
+// console.log(makeTransaction(1000, 3, 15000));
+// console.log(makeTransaction(5000, 10, 8000));
+// console.log(makeTransaction(2000, 8, 10000));
+// console.log(makeTransaction(500, 10, 5000));
+// // 19
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
 
-  if (password === null) {
-    message =  'Canceled by user!';
-  } else if (password === "jqueryismyjam") {
-    message = 'Welcome!';
-  } else {
-    message = 'Access denied, wrong password!';
-  }
-  return message;
-}
-console.log(checkPassword("mangohackzor"));
-console.log(checkPassword(null));
-console.log(checkPassword("polyhax"));
-console.log(checkPassword("jqueryismyjam"));
-// 20
-function checkStorage(available, ordered) {
-  let message;
-if (ordered === 0) {
-    message = "There are no products in the order!";
-  } else if (ordered > available) {
-    message = "Your order is too large, there are not enough items in stock!";
-  } else {
-    message = "The order is accepted, our manager will contact you";
-  }
-  return message;
-}
-console.log(checkStorage(100, 50));
-console.log(checkStorage(100, 130));
-console.log(checkStorage(70, 0));
-console.log(checkStorage(200, 20));
-console.log(checkStorage(200, 250));
+//   if (password === null) {
+//     message =  'Canceled by user!';
+//   } else if (password === "jqueryismyjam") {
+//     message = 'Welcome!';
+//   } else {
+//     message = 'Access denied, wrong password!';
+//   }
+//   return message;
+// }
+// console.log(checkPassword("mangohackzor"));
+// console.log(checkPassword(null));
+// console.log(checkPassword("polyhax"));
+// console.log(checkPassword("jqueryismyjam"));
+// // 20
+// function checkStorage(available, ordered) {
+//   let message;
+// if (ordered === 0) {
+//     message = "There are no products in the order!";
+//   } else if (ordered > available) {
+//     message = "Your order is too large, there are not enough items in stock!";
+//   } else {
+//     message = "The order is accepted, our manager will contact you";
+//   }
+//   return message;
+// }
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(70, 0));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 250));
+// // 21
+// function isNumberInRange(start, end, number) {
+//   const isInRange = (number >= start && number <= end);
+//   return isInRange;
+// }
+// console.log(isNumberInRange(10, 30, 17));
+// console.log(isNumberInRange(10, 30, 5));
+// console.log(isNumberInRange(20, 50, 24));
+// console.log(isNumberInRange(20, 50, 76));
+// 22
+// function checkIfCanAccessContent(subType) {
+//   const canAccessContent = (subType === "pro" || subType === "vip") ;
+//   return canAccessContent;
+// }
+// console.log(checkIfCanAccessContent("pro"));
+// console.log(checkIfCanAccessContent("starter"));
+// console.log(checkIfCanAccessContent("vip"));
+// console.log(checkIfCanAccessContent("free"));
+// 23
+// function isNumberNotInRange(start, end, number) {
+//   const isInRange = number >= start && number <= end;
+//   const isNotInRange = !isInRange;
+//   return isNotInRange;
+// }
+// console.log(isNumberNotInRange(10, 30, 17));
+// console.log(isNumberNotInRange(10, 30, 5));
+// console.log(isNumberNotInRange(20, 50, 24));
+// console.log(isNumberNotInRange(20, 50, 76));
+// 24
+// function getDiscount(totalSpent) {
+//   const BASE_DISCOUNT = 0;
+//   const BRONZE_DISCOUNT = 0.02;
+//   const SILVER_DISCOUNT = 0.05;
+//   const GOLD_DISCOUNT = 0.1;
+//   let discount;
+// if (totalSpent >= 50000) {
+//     discount = GOLD_DISCOUNT;
+//   } else if (50000 > totalSpent && totalSpent >= 20000) {
+//     discount = SILVER_DISCOUNT;
+//   } else if (20000 > totalSpent && totalSpent >= 5000) {
+//     discount = BRONZE_DISCOUNT;
+//   } else {discount = BASE_DISCOUNT; 
+//   }
+//   return discount;
+// }
+// console.log(getDiscount(137000));
+// console.log(getDiscount(46900));
+// console.log(getDiscount(8250));
+// console.log(getDiscount(1300));
+// console.log(getDiscount(5000));
+// console.log(getDiscount(20000));
+// console.log(getDiscount(50000));
+// 25

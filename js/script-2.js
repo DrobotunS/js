0df2e5186c2c51e2ce342b213553675ea82c525d
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-07-30 16:56:50 +0300

    add 9

---
+++ js/script-2.js
@@ -0,0 +1,67 @@
+// 1
+// function checkAge(age) {
+//   if (age >= 18) {
+//     return "You are an adult";
+//   }
+
+//   return "You are a minor";
+// }
+// console.log(checkAge(20));
+// console.log(checkAge(8));
+// console.log(checkAge(14));
+// console.log(checkAge(38));
+// 2
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+
+//   if (password === ADMIN_PASSWORD) {
+//     return "Welcome!";
+//   }
+// return "Access denied, wrong password!";
+  
+// }
+// console.log(checkPassword("mangohackzor"));
+// console.log(checkPassword("polyhax"));
+// console.log(checkPassword("jqueryismyjam"));
+// 3
+// function checkStorage(available, ordered) {
+//   if (ordered === 0) {
+//     return "Your order is empty!";
+//   } if (ordered > available) {
+//     return "Your order is too large, not enough goods in stock!";
+//   }
+//     return "The order is accepted, our manager will contact you";
+// }
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(70, 0));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 250));
+// console.log(checkStorage(150, 0));
+// 4
+// const fruits = ["apple", "plum", "pear", "orange"];
+// 5
+// const fruits = ["apple", "plum", "pear", "orange"];
+// const firstElement = fruits[0]
+// console.log(firstElement);
+// const secondElement = fruits[1];
+// console.log(secondElement);
+// const lastElement = fruits[3];
+// console.log(lastElement);
+// 6
+// const fruits = ["apple", "plum", "pear", "orange"];
+// fruits[1] = "peach";
+// fruits[3] = "banana";
+// console.log(fruits);
+// 7
+// const fruits = ["apple", "peach", "pear", "banana"];
+// const fruitsArrayLength = fruits.length;
+// console.log(fruits.length);
+// 8
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+// const lastElementIndex = fruits.length - 1;
+// console.log(lastElementIndex);
+//  const lastElement = fruits[lastElementIndex];
+//  console.log(lastElement);
+// 9

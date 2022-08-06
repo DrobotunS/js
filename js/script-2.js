f0444de88c4702148f440f4730278ec3914a4c07
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-08-06 18:14:46 +0300

    add28

--- js/script-2.js
+++ js/script-2.js
@@ -274,7 +274,7 @@
 //     for (let i = 0; i < words.length; i += 1) {
 //         if (words[i].length > maxlength.length) {
 //             maxlength = words[i];
-//         }     
+//         }
 //     }
             
 //     return maxlength;
@@ -290,4 +290,161 @@
 // Google
 // force
 // 22
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   // Change code below this line
+// for (let i = min; i <= max; i += 1) {
+//   numbers.push(i);
+// }
+  
+//   // Change code above this line
+//   return numbers;
+// }
+// console.log(createArrayOfNumbers(1, 3));
+// console.log(createArrayOfNumbers(14, 17));
+// console.log(createArrayOfNumbers(29, 34));
 
+// [ 1, 2, 3 ]
+// [ 14, 15, 16, 17 ]
+// [29, 30, 31, 32, 33, 34]
+
+// 23
+// function filterArray(numbers, value) {
+//    // Change code below this line
+// const number = [];
+//   for ( let i = 0; i < numbers[i]; i += 1){
+//     if (numbers[i] > value) {
+//       number.push(numbers[i]);
+//     }
+      
+//   }
+//   return number;
+
+//   // Change code above this line
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// [ 4, 5 ]
+// [ 5 ]
+// []
+// [ 41, 76 ]
+// [24, 41, 76]
+
+// 24
+// function checkFruit(fruit) {
+//   const fruits = ["apple", "plum", "pear", "orange"];
+
+//   return fruits.includes(fruit);
+// }
+// console.log(checkFruit("plum"));
+// console.log(checkFruit("mandarin"));
+// console.log(checkFruit("pear"));
+// console.log(checkFruit("Pear"));
+// console.log(checkFruit("apple"));
+
+// true
+// false
+// true
+// false
+// true
+
+// 25
+// function getCommonElements(array1, array2) {
+//   // Change code below this line
+// const element = [];
+//   for (let array of array1) {
+//     if (array1.includes(array) && array2.includes(array)) {
+//       element.push(array)
+//     }
+
+//   }
+//   return element;
+//  // Change code above this line
+// }
+// console.log(getCommonElements([1, 2, 3], [2, 4]));
+// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+// console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
+//  26
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+
+//   for (let orders of order) {
+//     total += orders;
+//   }
+
+//   // Change code above this line
+//   return total;
+// }
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+// console.log(calculateTotalPrice([]));
+
+// 138
+// 503
+// 1116
+// 0
+
+// 27
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   const filteredNumbers = [];
+
+//   for (let element of numbers) {
+//     const number = element;
+
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+
+//   return filteredNumbers;
+//   // Change code above this line
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// [ 4, 5 ]
+// [ 5 ]
+// []
+// [ 41, 76 ]
+// [ 24, 41, 76 ]
+
+// 28
+console.log(5 % 2);
+// 5, разделенное на 1, равно 5, а остаток - 0
+
+// 5 % 2 = 1;
+// //  5, разделенное на 2, равно 2, а остаток - 1
+
+// 5 % 3 = 2;
+// //  5, разделенное на 3, равно 1, а остаток - 2
+
+// 5 % 4 = 1;
+// //  5, разделенное на 4, равно 1, а остаток - 1
+
+// 5 % 5 = 0;
+// //  5, разделенное на 5, равно 1, а остаток - 0) 5 % 1 = 0;
+// // 5, разделенное на 1, равно 5, а остаток - 0
+
+// 5 % 2 = 1;
+// //  5, разделенное на 2, равно 2, а остаток - 1
+
+// 5 % 3 = 2;
+// //  5, разделенное на 3, равно 1, а остаток - 2
+
+// 5 % 4 = 1;
+// //  5, разделенное на 4, равно 1, а остаток - 1
+
+// 5 % 5 = 0;
+// //  5, разделенное на 5, равно 1, а остаток - 0

bafd081317d4af78d48636d8b0fb4e65b9ccbf8c
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-08-10 23:24:22 +0300

    add19

--- js/script-3.js
+++ js/script-3.js
@@ -401,3 +401,31 @@
 
 // 19
 
+const products = [
+    { name: "Radar", price: 1300, quantity: 4 },
+    { name: "Scanner", price: 2700, quantity: 3 },
+    { name: "Droid", price: 400, quantity: 7 },
+    { name: "Grip", price: 1200, quantity: 9 },
+  ];
+//   const productsName = [];
+//   const productsPrice = [];
+//   const productsQuantity = [];
+  function getAllPropValues(propName) {
+    for (const product of products) {
+                if (product.name === propName) {
+            return product.name;
+        }
+        return null;
+    }
+    
+}
+//    productsName.push(product.name);
+//     productsName.push(product.price);
+//       productsName.push(product.quantity);
+    
+
+    console.log(getAllPropValues("name"));
+    console.log(getAllPropValues("quantity"));
+    console.log(getAllPropValues("price"));
+  console.log(getAllPropValues("category"));
+  

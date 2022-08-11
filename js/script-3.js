5cbc723dcf9dae74d5b4901cbd81ee3f1d532e37
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-08-11 22:40:46 +0300

    add26

--- js/script-3.js
+++ js/script-3.js
@@ -490,3 +490,90 @@
 
 // 23
 
+// const highTemperatures = {
+//     yesterday: 28,
+//     today: 26,
+//     tomorrow: 33,
+//   }
+//   const { yesterday: highYesterday, today: highToday, tomorrow: highTomorrow, icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"} = highTemperatures;
+//   // Change code above this line
+//   const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+  
+//   console.log(highYesterday);
+//   console.log(highToday);
+//   console.log(highTomorrow);
+//   console.log(highIcon);
+
+// 28
+// 26
+// 33
+// https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg
+
+// 24
+// const colors = [
+//     { hex: "#f44336", rgb: "244,67,54" },
+//     { hex: "#2196f3", rgb: "33,150,243" },
+//     { hex: "#4caf50", rgb: "76,175,80" },
+//     { hex: "#ffeb3b", rgb: "255,235,59" },
+//   ];
+  
+//   const hexColors = [];
+//   const rgbColors = [];
+//   // Change code below this line
+  
+//   for (const { hex, rgb} of colors) { 
+//     hexColors.push(hex);
+//     rgbColors.push(rgb);
+//   }
+//   console.log(hexColors);
+//   console.log(rgbColors);
+
+//   [ '#f44336', '#2196f3', '#4caf50', '#ffeb3b' ]
+// [ '244,67,54', '33,150,243', '76,175,80', '255,235,59' ]
+
+// 25
+
+// const forecast = {
+//     today: {
+//       low: 28,
+//       high: 32,
+//       icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+//     },
+//     tomorrow: {
+//       low: 27,
+//       high: 31,
+//     },
+//   };
+//   // Change code below this line
+//   const {
+//     today: {low: lowToday, high: highToday, icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"},
+//     tomorrow: { low: lowTomorrow, high: highTomorrow, icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"},
+//   } = forecast;
+//   console.log(highToday);
+//   console.log(lowToday);
+//   console.log(todayIcon);
+  
+//   console.log(highTomorrow);
+//   console.log(lowTomorrow);
+//   console.log(tomorrowIcon);
+// 32
+// 28
+// https://www.flaticon.com/svg/static/icons/svg/861/861059.svg
+// 31
+// 27
+// https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg
+
+// 26
+
+// Change code below this line
+function calculateMeanTemperature(forecast) {
+    const todayLow = forecast.today.low;
+    const todayHigh = forecast.today.high;
+    const tomorrowLow = forecast.tomorrow.low;
+    const tomorrowHigh = forecast.tomorrow.high;
+  
+    // Change code above this line
+    return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+  }
+    console.log(calculateMeanTemperature({ today: {low: 28, high: 32}, tomorrow: {low: 25, high: 29} }));
+    console.log(calculateMeanTemperature({ today: {low: 37, high: 40}, tomorrow: {low: 33, high: 38} }));

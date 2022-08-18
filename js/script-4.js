d021cabd73042d1af663392342160bc84a4b0ec1
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-08-18 18:06:46 +0300

    add 36

--- js/script-4.js
+++ js/script-4.js
@@ -1534,17 +1534,20 @@
 // 673
 
 // 35
-const players = [
-  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
-  { name: "Poly", playtime: 469, gamesPlayed: 2 },
-  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
-  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
-];
-// Change code below this line
+// const players = [
+//   { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+//   { name: "Poly", playtime: 469, gamesPlayed: 2 },
+//   { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+//   { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+// ];
+// // Change code below this line
 
-const totalAveragePlaytimePerGame = players.reduce((total, player) => {
-  return total + player.playtime / Object.values(players);
-}, 0);;
+// const totalAveragePlaytimePerGame = players.reduce((total, player) => {
+//   return total + player.playtime / player.gamesPlayed;
+// }, 0);;
 
+// console.log(totalAveragePlaytimePerGame);
 
-console.log(totalAveragePlaytimePerGame);
+// 1023
+
+// 36
